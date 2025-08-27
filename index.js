66b67bdfdf3e440ded36dd0ec10548a5901f0691
Muhammad Iyazuz Zidan <iyazuzzidan@gmail.com>
2025-08-27 21:25:01 +0700

    configure cors and body parser

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 //import express
 const express = require('express')
 
+//import CORS
+const cors = require('cors')
+
+//import bodyParser
+const bodyParser = require('body-parser')
+
 //init app
 const app = express()
+
+//use cors
+app.use(cors())
+
+//use body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 //define port
 const port = 3000;
